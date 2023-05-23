cc91b87d6997bd06eeb76c28ae1c5e1c0b61558f
Alex C.Y. Zhang <chzhang@sissa.it>
2023-05-23 16:16:44 +0200

    removed multithreading from VEM_experimnets.jl

--- src/VEM_experiments.jl
+++ src/VEM_experiments.jl
@@ -6,7 +6,7 @@ name = "chr2_1000_66000_100k.txt"
 name_2 = "chr2_1000_66000_100k_oe_gf2.txt"
 println(name)
 println(name_2)
-Threads.@threads for i_classes in 1:length(ks)
+for i_classes in 1:length(ks)
 	run_inference_gauss(niters[i_classes], 1, 650, 8, "data/input/X_"*name, "data/input/Y_log_"*name_2, ks[i_classes])
 	#run_inference(niters[i_classes], 1, 1000, 10, "data/input/X_gaussian_noise_95_105.txt", "data/input/Y_bin_"*name, ks[i_classes])
 end
