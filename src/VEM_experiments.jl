cfc6859774c5c7ce5cbebb5de7592f196b9fe50b
Alex C.Y. Zhang <chzhang@sissa.it>
2023-07-18 11:49:24 +0200

    modified file to setup experiments

--- src/VEM_experiments.jl
+++ src/VEM_experiments.jl
@@ -1,15 +1,21 @@
 include("VEM_main.jl")
- ks = [8]#, 7, 8]
- niters = [300]#, 600, 600]
+ ks = [12]#, 7, 8]
+ niters = [350]#, 600, 600]
 
-name = "chr2_1000_66000_100k_4.txt"
-name_2 = "chr2_1000_66000_100k_oe_gf2.txt"
-println(name)
-println(name_2)
-for i_classes in 1:length(ks)
-	run_inference_gauss(niters[i_classes], 1, 500, 1, "data/input/X_"*name, "data/input/Y_log_"*name_2,ks[i_classes])
-	#run_inference_gauss(niters[i_classes], 1, 500, 4, "data/input/Y_log_"*name_2, ks[i_classes])
+names = ["chr2_1000_46000_100k.txt", "chr2_10000_55000_100k.txt", "chr2_19000_64000_100k.txt",
+		"chr2_28000_73000_100k.txt", "chr2_37000_82000_100k.txt"]
+names_2 = ["chr2_1000_46000_100k_oe_uf3.txt", "chr2_10000_55000_100k_oe_uf3.txt", "chr2_19000_64000_100k_oe_uf3.txt",
+			"chr2_28000_73000_100k_oe_uf3.txt", "chr2_37000_82000_100k_oe_uf3.txt"]
+for i_name in 1:length(names)
+	name = names[i_name]
+	name_2 = names_2[i_name]
+	println(name)
+	println(name_2)
+	for i_classes in 1:length(ks)
+		run_inference_gauss(niters[i_classes], 1, 500, 1, "data/input/X_"*name, "data/input/Y_log_"*name_2,ks[i_classes])
+		#run_inference_gauss(niters[i_classes], 1, 500, 4, "data/input/Y_log_"*name_2, ks[i_classes])
 
-	#run_inference(niters[i_classes], 1, 1000, 10, "data/input/X_gaussian_noise_95_105.txt", "data/input/Y_bin_"*name, ks[i_classes])
-end
+		#run_inference(niters[i_classes], 1, 1000, 10, "data/input/X_gaussian_noise_95_105.txt", "data/input/Y_bin_"*name, ks[i_classes])
+	end
 #end
+end
