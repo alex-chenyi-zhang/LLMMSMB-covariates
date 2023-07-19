493dd480e601c52d56d12bbd16539d240f52c808
Alex C.Y. Zhang <chzhang@sissa.it>
2023-07-19 10:00:33 +0200

    .

--- src/VEM_experiments.jl
+++ src/VEM_experiments.jl
@@ -1,18 +1,18 @@
 include("VEM_main.jl")
  ks = [16]#, 7, 8]
- niters = [350]#, 600, 600]
+ niters = [400]#, 600, 600]
 
-names = ["chr2_1000_46000_100k.txt", "chr2_10000_55000_100k.txt", "chr2_19000_64000_100k.txt",
-		"chr2_28000_73000_100k.txt", "chr2_37000_82000_100k.txt"]
-names_2 = ["chr2_1000_46000_100k_oe_gf2.txt", "chr2_10000_55000_100k_oe_gf2.txt", "chr2_19000_64000_100k_oe_gf2.txt",
-			"chr2_28000_73000_100k_oe_gf2.txt", "chr2_37000_82000_100k_oe_gf2.txt"]
+names = ["chr2_1000_61000_100k.txt", "chr2_3000_63000_100k.txt", "chr2_5000_65000_100k.txt",
+		"chr2_11000_71000_100k.txt"]
+names_2 = ["chr2_1000_61000_100k_oe_gf2.txt", "chr2_3000_63000_100k_oe_gf2.txt", "chr2_5000_65000_100k_oe_gf2.txt",
+			"chr2_11000_71000_100k_oe_gf2.txt"]
 for i_name in 1:length(names)
 	name = names[i_name]
 	name_2 = names_2[i_name]
 	println(name)
 	println(name_2)
 	for i_classes in 1:length(ks)
-		run_inference_gauss(niters[i_classes], 1, 450, 8, "data/input/X_"*name, "data/input/Y_log_"*name_2,ks[i_classes])
+		run_inference_gauss(niters[i_classes], 1, 600, 8, "data/input/X_"*name, "data/input/Y_log_"*name_2,ks[i_classes])
 		#run_inference_gauss(niters[i_classes], 1, 500, 4, "data/input/Y_log_"*name_2, ks[i_classes])
 
 		#run_inference(niters[i_classes], 1, 1000, 10, "data/input/X_gaussian_noise_95_105.txt", "data/input/Y_bin_"*name, ks[i_classes])
