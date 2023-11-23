7513ee313948cd2626fcbf2b7b752d6f6e583b86
Alex C.Y. Zhang <chzhang@sissa.it>
2023-11-23 14:30:53 +0100

    changed the run_multi.jl file

--- run_multi.jl
+++ run_multi.jl
@@ -1,13 +1,14 @@
 include("src/VEM_main.jl")
 
-Threads.@threads for R in [0.05, 0.1, 0.2, 0.5, 1.0 , 2.0]
-	run_inference_gauss_multi_NN(350, 1, 400, 6, "data/X_2_files_chr2_100k_2.txt", "data/Y_files_chr2_100k_2.txt", 8, R)
+Threads.@threads for R in [0.0001, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0]
+	run_inference_gauss_multi_NN(350, 1, 400, 6, "data/X_2_files_chr2_100k_2.txt", "data/Y_files_chr2_100k_2.txt", 14, R)
 end
 
-Threads.@threads for R in [0.05, 0.1, 0.2, 0.5, 1.0 , 2.0]
-	run_inference_gauss_multi(350, 1, 400, 6, "data/X_2_files_chr2_100k_2.txt", "data/Y_files_chr2_100k_2.txt", 8, R)
+Threads.@threads for R in [0.0001, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0]
+	run_inference_gauss_multi(350, 1, 400, 6, "data/X_2_files_chr2_100k_2.txt", "data/Y_files_chr2_100k_2.txt", 16, R)
 end
 
+#=
 Threads.@threads for R in [0.05, 0.1, 0.2, 0.5, 1.0 , 2.0]
 	run_inference_gauss_multi_NN(350, 1, 400, 6, "data/X_2_files_chr2_100k_2.txt", "data/Y_files_chr2_100k_2.txt", 10, R)
 end
@@ -22,4 +23,4 @@ end
 
 Threads.@threads for R in [0.05, 0.1, 0.2, 0.5, 1.0 , 2.0]
 	run_inference_gauss_multi(350, 1, 400, 6, "data/X_2_files_chr2_100k_2.txt", "data/Y_files_chr2_100k_2.txt", 12, R)
-end
+end=#
