ad439f189ad0a337565d2e1db0bbde5d0270bba8
Alex C.Y. Zhang <chzhang@sissa.it>
2024-02-05 12:03:07 +0100

    reordered some files and added input files for chromosomes 2,10,14 at 100k resolution, all data in in the p-arm of the chromosome

--- src/imr90_bulk_production.jl
+++ src/imr90_bulk_production.jl
@@ -1,10 +1,12 @@
 include("src/VEM_main.jl")
 
 R = 0.1
-Ks = [2, 6, 10, 12, 14]
-iters = [300, 300, 350, 350, 400]
-Threads.@threads for i in 1:5
-    run_inference_gauss_multi_NN(iters[i], 6, "data/X_files_IMR90_chr21_50k.txt", "data/Y_files_IMR90_chr21_50k.txt", Ks[i], R)
+Ks = [2, 3, 5, 6, 10, 12]
+iters = [250, 250, 300, 300, 350, 350]
+chrs = [2, 10, 14]
+dir = "data/IMR90_input_files/"
+for i in 1:6
+    Threads.@threads for chr in chrs
+        run_inference_gauss_multi_NN(iters[i], 6, "$(dir)X_files_IMR90_chr$(chr)_100k.txt", "$(dir)Y_files_IMR90_chr$(chr)_100k.txt", Ks[i], R)
+    end
 end
-
-run_inference_gauss_multi_NN(400, 6, "data/X_files_IMR90_chr2_250k.txt", "data/Y_files_IMR90_chr2_250k.txt", 14, R)
