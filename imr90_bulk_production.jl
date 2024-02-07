f7a68b05e72032c575b56ee8eb036b5657f35d51
Alex C.Y. Zhang <chzhang@sissa.it>
2024-02-07 16:26:44 +0100

    ..

--- imr90_bulk_production.jl
+++ imr90_bulk_production.jl
@@ -7,8 +7,8 @@ chrs = [2, 10, 14, 21]
 dir = "data/12878_input_files/"
 res = ["_100k.txt", "_100k.txt", "_100k.txt", "_50k.txt"]
 
-for i in 1:6
-    Threads.@threads for j in 4    
+Threads.@threads for j in 1:4 
+    for i in 1:6       
     	run_inference_gauss_multi_NN(iters[i], 6, "$(dir)X_files_12878_chr$(chrs[j])$(res[j])", "$(dir)Y_files_12878_chr$(chrs[j])$(res[j])", Ks[i], R)
     end
 end
