188b8509b9372efdf62611afed86d1a101e76e13
Alex C.Y. Zhang <chzhang@sissa.it>
2024-02-13 21:47:28 +0100

    added files and scripts for larger chromosomes at lower resolution

--- gm12878_batch.jl
+++ gm12878_batch.jl
@@ -2,11 +2,11 @@ include("src/VEM_main.jl")
 
 R = 0.1
 Ks = [2, 3, 4, 5, 6, 10, 12]
-iters = [250, 250, 250, 300, 300, 350, 350]
+iters = [200, 200, 200, 250, 250, 300, 300]
 dirX = "data/12878_input_files/X_files_12878"
 dirY = "data/12878_input_files/Y_files_12878"
 
-suffix = ["_even_100k.txt", "_odd_100k.txt", "_even_100k_nofilt.txt", "_odd_100k_nofilt.txt",]
+suffix = ["_even_500k.txt", "_odd_500k.txt", "_even_500k_nofilt.txt", "_odd_500k_nofilt.txt",]
 
 Threads.@threads for j in 1:4
     for i in 1:7
--- gm12878_single_chroms.jl
+++ gm12878_single_chroms.jl
@@ -2,12 +2,12 @@ include("src/VEM_main.jl")
 
 R = 0.1
 Ks = [2, 3, 4, 5, 6, 10, 12]
-iters = [250, 250, 250, 300, 300, 350, 350]
-chrs = [12, 13, 14, 15, 16, 17, 18, 19, 20 , 21 ,22]
+iters = [200, 200, 200, 250, 250, 300, 300]
+chrs = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10 , 11]
 dirX = "data/12878_input_files/X_files_12878_chr"
 dirY = "data/12878_input_files/Y_files_12878_chr"
 
-res = ["_100k.txt", "_100k.txt", "_100k.txt", "_100k.txt", "_100k.txt", "_100k.txt", "_100k.txt", "_100k.txt", "_100k.txt", "_100k.txt", "_100k.txt"]
+res = ["_500k.txt", "_500k.txt", "_500k.txt", "_500k.txt", "_500k.txt", "_500k.txt", "_500k.txt", "_500k.txt", "_500k.txt", "_500k.txt", "_500k.txt"]
 
 Threads.@threads for j in 1:11
     for i in 1:7
