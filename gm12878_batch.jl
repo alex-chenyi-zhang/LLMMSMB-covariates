13cb3d246a9ce5265cdea70362ce6f067d6f15ca
Alex C.Y. Zhang <chzhang@sissa.it>
2024-02-16 14:31:50 +0100

    hellow!

--- gm12878_batch.jl
+++ gm12878_batch.jl
@@ -6,10 +6,10 @@ iters = [200, 200, 200, 300, 300, 300, 350, 350]
 dirX = "data/12878_input_files/X_files_12878"
 dirY = "data/12878_input_files/Y_files_12878"
 
-suffix = ["_even_250k.txt", "_odd_250k.txt", "_even_250k_nofilt.txt", "_odd_250k_nofilt.txt",]
+suffix = ["_16even22_100k.txt", "_15odd21_100k.txt", "_16even22_100k_nofilt.txt", "_15odd21_100k_nofilt.txt",]
 
 Threads.@threads for j in 1:4
     for i in 1:8
-        run_inference_gauss_multi_NN(iters[i], 6, "$(dirX)$(suffix[j])", "$(dirY)$(suffix[j])", Ks[i], R)
+        run_inference_gauss_multi_NN(iters[i], 12, "$(dirX)$(suffix[j])", "$(dirY)$(suffix[j])", Ks[i], R)
     end
 end
